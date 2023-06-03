2206115cc8d04ca18f54d620463bced40af72935
Krystian Jachna <krysti4nape@gmail.com>
2023-06-04 00:54:37 +0200

    added _regex_to_enfa method in eNFA class and building eNFA method prototype

--- regex/automata.py
+++ regex/automata.py
@@ -10,7 +10,33 @@ class eNFA:
         self.start_state: int | None = None
         self.end_state: int | None = None
 
+    def _regex_to_enfa(self, regex: str):
+        parsed_regex = parse(regex)
+        start_state = self._create_state()
+        self.states.add(start_state)
+        self.start_state = start_state
+        self.end_state = self._build_enfa(parsed_regex.root, self.start_state)
 
+    def _build_enfa(self, node: dict, start_state: int) -> int:
+
+
+        if node["operator"] is not None:
+            pass # TODO
+
+        if node["type"] == "symbol":
+           pass # TODO
+
+        elif node["type"] == "alternative":
+            pass # TODO
+
+        elif node["type"] == "concatenation":
+            pass # TODO
+
+        elif node["type"] == "special_symbol":
+            pass # TODO
+
+        else:
+            raise ValueError("Invalid node type: " + node["type"])
 
 
 class NFA:
