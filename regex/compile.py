21b112473749ad6b65c306e69a7aaf94fe4b2351
Mateusz Kadula <mateusz.kadula@icloud.com>
2023-06-09 00:36:08 +0200

    additional methods for CompiledRegex and tests

--- regex/compile.py
+++ regex/compile.py
@@ -1,10 +1,12 @@
+"""Regex toolset"""
 
-from typing import Self, Union
+from typing import Self, Union, Any
 from pickle import dumps, loads
 from regex.automata import ENFA, NFA, DFA
 
 
 class Match:
+    """Stores one substring of a text that belongs to the language expressed in regex"""
 
     def __init__(self, text: str, span: tuple[int, int], __reg: 'CompiledRegex' = None):
         self.text = text
@@ -13,6 +15,11 @@ class Match:
 
     def __repr__(self):
         return f"<Match: {repr(self.get_str)}, span: {self.span}>"
+
+    def __eq__(self, other: Any) -> bool:
+        if not isinstance(other, type(self)):
+            return False
+        return self.text == other.text and self.span == other.span
 
     @property
     def begin(self) -> int:
@@ -32,6 +39,7 @@ class Match:
 
 
 class CompiledRegex:
+    """Compile a regular expression"""
 
     def __init__(self, regular_expression: str):
         self.regex = regular_expression
@@ -46,6 +54,10 @@ class CompiledRegex:
         return f"compile.CompiledRegex({repr(self.regex)})"
 
     def full_match(self, text: str) -> Match | None:
+        """
+        Returns Match object if the entire text matches the regular expression,
+        returns None otherwise.
+        """
         current_state = self.dfa.start_state
         for letter in text:
             if letter not in self.dfa.alphabet:
@@ -58,14 +70,68 @@ class CompiledRegex:
         else:
             return None
 
-    def match_all(self, text: str) -> list[Match]:
+    def match(self, text: str) -> Match | None:
+        """
+        Returns Match object if the beginning of text matches the regular expression,
+        returns None otherwise.
+        """
+        last_end_state = None if self.dfa.start_state not in self.dfa.end_states else -1
+        current_state = self.dfa.start_state
+        for i, letter in enumerate(text):
+            next_state = self.dfa.transitions.get((current_state, letter), None)
+            if next_state is None or next_state == self.dfa.sink_state:
+                break
+            if next_state in self.dfa.end_states:
+                last_end_state = i
+            current_state = next_state
+        return Match(text, (0, last_end_state + 1), self) if last_end_state is not None else None
+
+    def search(self, text: str) -> Match | None:
+        """
+        Returns the first substring in text that matches the regular expression,
+        returns None if no such substring is found.
+        """
         automatons: list[(int, int, int)] = []
         next_automatons: list[(int, int, int)] = []
         for i, letter in enumerate(text):
             next_automatons.clear()
 
             if self.dfa.start_state not in self.dfa.end_states:
-                automatons.append((i, -1, self.dfa.start_state))
+                automatons.append((i, None, self.dfa.start_state))
+            else:
+                automatons.append((i, i - 1, self.dfa.start_state))
+
+            for index, last, state in automatons:
+                next_state = self.dfa.transitions.get((state, letter), None)
+                if next_state in self.dfa.end_states:
+                    next_automatons.append((index, i, next_state))
+                    break
+                elif next_state is not None and next_state != self.dfa.sink_state:
+                    next_automatons.append((index, last, next_state))
+                elif last is not None:
+                    return Match(text, (index, last + 1), self)
+
+            automatons = next_automatons.copy()
+
+        if len(automatons) > 0:
+            index, last, _ = automatons[0]
+            if last is None:
+                return None
+            return Match(text, (index, last + 1), self)
+        else:
+            return None
+
+    def find_all(self, text: str) -> list[Match]:
+        """
+        Returns a list of all substrings that match the regular expression.
+        """
+        automatons: list[(int, int, int)] = []
+        next_automatons: list[(int, int, int)] = []
+        for i, letter in enumerate(text):
+            next_automatons.clear()
+
+            if self.dfa.start_state not in self.dfa.end_states:
+                automatons.append((i, None, self.dfa.start_state))
             else:
                 automatons.append((i, i - 1, self.dfa.start_state))
 
@@ -77,16 +143,18 @@ class CompiledRegex:
                 elif next_state is not None and next_state != self.dfa.sink_state:
                     next_automatons.append((index, last, next_state))
                 else:
-                    if last != -1:
+                    if last is not None:
                         next_automatons.append((index, last, -1))
 
             automatons = next_automatons.copy()
 
-        return [Match(text, (start, stop + 1), self) for start, stop, _ in automatons if stop != -1]
+        return [Match(text, (start, stop + 1), self) for start, stop, _ in automatons if stop is not None]
 
     def pack(self) -> bytes:
+        """Returns a serialised version of Self that can be stored in a file."""
         return dumps(self)
 
     @classmethod
     def unpack(cls, contents: bytes) -> Self:
+        """Returns an instance of class serialised by self.pack()"""
         return loads(contents)
---
+++ tests/compile.py
@@ -0,0 +1,219 @@
+"""Tests for regex/compile.py"""
+
+import unittest as ut
+from regex.compile import CompiledRegex, Match
+
+
+class CompileTest(ut.TestCase):
+
+    def test_match(self):
+        reg = CompiledRegex(r"a+b+")
+        text = "aaaaabbbbaaababaaaabbbb"
+        self.assertEqual(
+            reg.match(text),
+            Match(text, (0, 9))
+        )
+        reg = CompiledRegex(r"[ab]*c[ab]*")
+        text = "aaaaabbbbaacababdaaaabbbb"
+        self.assertEqual(
+            reg.match(text),
+            Match(text, (0, 16))
+        )
+        reg = CompiledRegex(r"\s*(www\.)?[-a-zA-Z]+\.")
+        text = "   www.Git-Hub.com.pl"
+        self.assertEqual(
+            reg.match(text),
+            Match(text, (0, 15))
+        )
+        text = " Git-Hub.com.pl"
+        self.assertEqual(
+            reg.match(text),
+            Match(text, (0, 9))
+        )
+        reg = CompiledRegex(r"a+b+")
+        text = "www.Git-Hub.com.pl"
+        self.assertEqual(
+            None,
+            reg.match(text)
+        )
+        reg = CompiledRegex(r"a*b*")
+        text = "www.Git-Hub.com.pl"
+        self.assertEqual(
+            reg.match(text),
+            Match(text, (0, 0))
+        )
+
+    def test_find_iter(self):
+        reg = CompiledRegex(r"a+b+")
+        text = """
+aaaabbbaaab
+aabbaaabbb
+abbab
+"""
+        self.assertEqual(
+            reg.find_all(text),
+            [
+                Match(text, (1, 8)),
+                Match(text, (8, 12)),
+                Match(text, (13, 17)),
+                Match(text, (17, 23)),
+                Match(text, (24, 27)),
+                Match(text, (27, 29)),
+            ]
+        )
+        reg = CompiledRegex(r"a*b*")
+        text = """
+aaaabbbaaab
+aabbaaabbb
+abbab
+"""
+        self.assertEqual(
+            reg.find_all(text),
+            [
+                Match(text, (0, 0)),
+                Match(text, (1, 8)),
+                Match(text, (8, 12)),
+                Match(text, (12, 12)),
+                Match(text, (13, 17)),
+                Match(text, (17, 23)),
+                Match(text, (23, 23)),
+                Match(text, (24, 27)),
+                Match(text, (27, 29)),
+                Match(text, (29, 29)),
+            ]
+        )
+        reg = CompiledRegex(r"([a-z0-9_\.]+)@([-\da-z\.]+)\.([a-z\.]{2,6})")
+        text = '''
+adam.kowalski@gmail.pl
+grzegorz_brzeczyszczykiewicz@icloud.com
+walter1978white@mk.pl
+notAnEmail@gmail.io
+notanemailalsogmail.com
+'''
+        self.assertEqual(
+            reg.find_all(text),
+            [
+                Match(text, (1, 23)),
+                Match(text, (24, 63)),
+                Match(text, (64, 85)),
+                Match(text, (92, 105)),
+            ]
+        )
+        reg = CompiledRegex(r"b[ab]*")
+        text = 'aabbbbbaaabbaaabbbba'
+        self.assertEqual(
+            reg.find_all(text),
+            [
+                Match(text, (2, len(text)))
+            ]
+        )
+        reg = CompiledRegex(r".*")
+        text = 'ocwei uhwp98y24p985h;i4ubv;wej wv fkjew '
+        self.assertEqual(
+            reg.find_all(text),
+            [
+                Match(text, (0, len(text)))
+            ]
+        )
+        reg = CompiledRegex(r"|")
+        text = 'ocw'
+        self.assertEqual(
+            reg.find_all(text),
+            [
+                Match(text, (0, 0)),
+                Match(text, (1, 1)),
+                Match(text, (2, 2))
+            ]
+        )
+
+    def test_match_full(self):
+        reg = CompiledRegex(r"ocw")
+        text = 'ocw'
+        self.assertTrue(reg.full_match(text) is not None)
+        reg = CompiledRegex(r"a+b+")
+        text = 'aaaaaabbbbbbb'
+        self.assertTrue(reg.full_match(text) is not None)
+        reg = CompiledRegex(r"a+b+")
+        text = 'aaaaaabbbbbbba'
+        self.assertTrue(reg.full_match(text) is None)
+        reg = CompiledRegex(r"([a-z0-9_\.]+)@([-\da-z\.]+)\.([a-z\.]{2,6})")
+        text = 'suspicious_tech_support@not-a-scam.edu.pl'
+        self.assertTrue(reg.full_match(text) is not None)
+        reg = CompiledRegex(r"([a-z0-9_\.]+)@([-\da-z\.]+)\.([a-z\.]{2,6})")
+        text = 'normal_email_addressATgmail.com'
+        self.assertTrue(reg.full_match(text) is None)
+        reg = CompiledRegex(r"(www\.)?[-A-Za-z0-9_\.]+\.(com|pl|io)")
+        text = 'www.website_ai.pl.com'
+        self.assertTrue(reg.full_match(text) is not None)
+        reg = CompiledRegex(r"(www\.)?[-A-Za-z0-9_\.]+\.(com|pl|io)")
+        text = 'website_ai.pl.com'
+        self.assertTrue(reg.full_match(text) is not None)
+        reg = CompiledRegex(r"(www\.)?[-A-Za-z0-9_\.]+\.(com|pl|io)")
+        text = 'www.website_ai.pl.co'
+        self.assertTrue(reg.full_match(text) is None)
+
+    def test_search(self):
+        reg = CompiledRegex(r"(www\.)?[-A-Za-z0-9_\.]+\.(com|pl|io)")
+        text = 'this is my website: www.website_ai.pl.co end of website'
+        self.assertEqual(
+            reg.search(text),
+            Match(text, (20, 37))
+        )
+        reg = CompiledRegex(r"\s(www\.)?[-A-Za-z0-9_\.]+\.(com|pl|io)")
+        text = 'this is my email: walter_bialy@gmail.com end of email'
+        self.assertEqual(
+            reg.search(text),
+            None
+        )
+        reg = CompiledRegex(r"a+")
+        text = 'bbbbbbbbaaaaaa'
+        self.assertEqual(
+            reg.search(text),
+            Match(text, (8, 14))
+        )
+        reg = CompiledRegex(r"a+")
+        text = 'aaabbbbbbbaaaaaaaabbbbaaaaaaaaaaaaaa'
+        self.assertEqual(
+            reg.search(text),
+            Match(text, (0, 3))
+        )
+        reg = CompiledRegex(r"a+")
+        text = 'aaabbbbbbbaaaaaaaabbbbaaaaaaaaaaaaaa'
+        self.assertEqual(
+            reg.search(text),
+            Match(text, (0, 3))
+        )
+        reg = CompiledRegex(r"g[a-zA-Z]*d")
+        text = """
+        According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small 
+        to get its fat little body off the ground. The bee, of course, flies anyway because bees don't care what humans
+        think is impossible. Yellow, black. Yellow, black. Yellow, black. Yellow, black. Ooh, black and yellow!"""
+        self.assertEqual(
+            reg.search(text),
+            Match(text, (163, 169))
+        )
+        reg = CompiledRegex(r"\sy[a-zA-Z]*o\s")
+        text = """
+        According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small 
+        to get its fat little body off the ground. The bee, of course, flies anyway because bees don't care what humans
+        think is impossible. Yellow, black. Yellow, black. Yellow, black. Yellow, black. Ooh, black and yellow! yoo """
+        self.assertEqual(
+            reg.search(text),
+            Match(text, (351, 356))
+        )
+        reg = CompiledRegex(r"this is not in the text")
+        self.assertEqual(
+            reg.search(text),
+            None
+        )
+        reg = CompiledRegex(r"what humans\n        think is impossible")
+        self.assertEqual(
+            reg.search(text),
+            Match(text, (228, 267))
+        )
+        reg = CompiledRegex(r"a*")
+        text = ' '
+        self.assertEqual(
+            reg.search(text),
+            Match(text, (0, 0))
+        )
